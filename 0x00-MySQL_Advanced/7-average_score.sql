5ab445829133c8c4b73d6d4373ca219445ed7528
Menna Hassan <h.menna85@gmail.com>
2024-03-21 22:09:03 +0200

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,7 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE IF NOT EXISTS ComputeAverageScoreForUser (userId INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (userId INT)
 BEGIN
     UPDATE users
     SET average_score = (SELECT AVG(score)
