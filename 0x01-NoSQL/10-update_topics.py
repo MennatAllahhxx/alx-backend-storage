6a8c7480d6cae6d297befdcb0722bc0496f71fa1
Menna Hassan <h.menna85@gmail.com>
2024-03-27 02:20:16 +0200

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,14 +3,15 @@
 Module contains update_topics fun
 """
 
+from typing import List
 
-def update_topics(mongo_collection, name, topics):
-    """AI is creating summary for insert_school
+
+def update_topics(mongo_collection, name:str, topics: List[str]):
+    """AI is creating summary for update_topics
 
     Args:
-        mongo_collection (mongoDB collection): to insert new docs to.
-
-    Returns:
-        str: new docs id
+        mongo_collection (collection): to update docs in.
+        name (str): name of the doc
+        topics (List[str]): topics to be updated
     """
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
