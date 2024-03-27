2391a848c4c86823f886123d4714bff4b807ba8c
Menna Hassan <h.menna85@gmail.com>
2024-03-27 03:36:22 +0200

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 """
-script that provides some stats about Nginx logs stored in MongoDB
+Module contains main fun
 """
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
-db = client.logs
-collection = db.nginx
-print("{} logs".format(collection.count_documents({})))
-print("Methods:")
-print("\t method GET: {}".format(collection.
-                                 count_documents({"method": "GET"})))
-print("\t method POST: {}".format(collection.
-                                  count_documents({"method": "POST"})))
-print("\t method PUT: {}".format(collection.
-                                 count_documents({"method": "PUT"})))
-print("\t method PATCH: {}".format(collection.
-                                   count_documents({"method": "PATCH"})))
-print("\t method DELETE: {}".format(collection.
-                                    count_documents({"method": "DELETE"})))
-print("{} status check".format(collection.
-                               count_documents({
-                                                "method": "GET",
-                                                "path": "/status"
-                                                })))
+
+def main():
+    """
+    script that provides some stats about Nginx logs stored in MongoDB
+    """
+    client = MongoClient("mongodb://localhost:27017")
+    db = client.logs
+    collection = db.nginx
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    print("\t method GET: {}".format(collection.
+                                     count_documents({"method": "GET"})))
+    print("\t method POST: {}".format(collection.
+                                      count_documents({"method": "POST"})))
+    print("\t method PUT: {}".format(collection.
+                                     count_documents({"method": "PUT"})))
+    print("\t method PATCH: {}".format(collection.
+                                       count_documents({"method": "PATCH"})))
+    print("\t method DELETE: {}".format(collection.
+                                        count_documents({"method": "DELETE"})))
+    print("{} status check".format(collection.
+                                   count_documents({
+                                                    "method": "GET",
+                                                    "path": "/status"
+                                                    })))
+
+
+if __name__ == "__main__":
+    main()
